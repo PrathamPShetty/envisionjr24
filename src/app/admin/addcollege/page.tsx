c963721a1cc616b57f903f4b9fc48f9e7461f1e0
PrathamPShetty <prathampshetty99sai@gmail.com>
2024-12-14 02:37:31 +0530

    COMPLETED

--- src/app/admin/addcollege/page.tsx
+++ src/app/admin/addcollege/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 // Define the structure of a college object
--- src/components/college.tsx
+++ src/components/college.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 // Define the structure of a college object
